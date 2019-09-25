9c8f84036072d89de83a14b163d8d3e7c8e0d136
Igor Plotnikov <ig.plotnikov@gmail.com>
2019-09-26 01:18:24 +0300

    remove routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'invoices#index'
 
-  resources :invoices, param: :token
+  resources :invoices, only: [:show, :create], param: :token
 
-  resources :drugs, param: :token do
-    resources :serts
+  resources :drugs, only: [:show, :new, :crerate], param: :token do
+    resources :serts, only: [:create]
 
     get :download, on: :member
   end
