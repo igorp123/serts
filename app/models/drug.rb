ed2318cf1dbf52ba7d85299a0fa29df2fa98b720
Igor Plotnikov <ig.plotnikov@gmail.com>
2019-09-29 22:37:52 +0300

    rescue to ftp_read

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -64,28 +64,38 @@ private
   end
 
   def get_serts_from_ftp
-    Net::FTP.open('91.239.68.66',
-                   Rails.application.credentials.dig(:ftp, :login),
-                   Rails.application.credentials.dig(:ftp, :password)) do |ftp|
-      ftp.chdir(self.sert_path)
-      files = ftp.nlst
-
-      files.each_with_index do |file, index|
-        file_extension = file.split('.').last
-
-        if IMAGE_TYPES.include? file_extension
-          local_file_name = "#{TMP_PATH}image_#{index + 1}.#{file_extension}"
-
-          ftp.getbinaryfile(file, local_file_name)
-
-          sert_file = File.open(local_file_name)
-
-          self.serts.build(sert: sert_file).save
-
-          File.delete(local_file_name)
+    begin
+      Net::FTP.open('91.239.68.66',
+                     Rails.application.credentials.dig(:ftp, :login),
+                     Rails.application.credentials.dig(:ftp, :password)) do |ftp|
+        begin
+          ftp.chdir(self.sert_path)
+        rescue Net::FTPPermError
+          abort 'dir has not been found'
         end
-      end
+
+        files = ftp.nlst
+
+        files.each_with_index do |file, index|
+          file_extension = file.split('.').last
+
+          if IMAGE_TYPES.include? file_extension
+            local_file_name = "#{TMP_PATH}image_#{index + 1}.#{file_extension}"
+
+            ftp.getbinaryfile(file, local_file_name)
+
+            sert_file = File.open(local_file_name)
+
+            self.serts.build(sert: sert_file).save
+
+            File.delete(local_file_name)
+          end
+        end
+
       ftp.close
+      end
+    rescue Errno::ETIMEDOUT
+      abort 'Не удалось подключиться к ftp серверу'
     end
   end
 end
