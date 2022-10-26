65d67af5a4db753b9e93d3f790fd049c9c2b24f6
Igor Plotnikov <ig.plotnikov@gmail.com>
2022-10-26 13:00:04 +0300

    Change ftp setting for local network

--- app/service/read_invoices_ftp.rb
+++ app/service/read_invoices_ftp.rb
@@ -3,11 +3,12 @@ require 'net/ftp'
 class ReadInvoicesFtp
   FILE_NAME = 'public/invoices.json'
   JSON_FOLDER = 'json'
-  FILE = 'invoices.json'
+  FILE = 'inv_uas.json'
 
   def self.call
     begin
-      Net::FTP.open('91.239.68.66',
+      # 91.239.68.66
+      Net::FTP.open('192.168.137.40',
                     Rails.application.credentials.dig(:ftp, :login),
                     Rails.application.credentials.dig(:ftp, :password)) do |ftp|
 
