901cc6aded9bf12d0e665db0321f64135fcb16cd
Igor Plotnikov <ig.plotnikov@gmail.com>
2022-11-14 11:44:43 +0300

    change whenever sheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
-every 1.day, at: '0:15 am' do
+every 3.hours, at: '0:10 am' do
   runner "ReadInvoicesFtp.call"
 end
 
-every 1.day, at: '0:20 am' do
+every 3.hours, at: '0:15 am' do
   runner "ReadSerts.call"
 end
